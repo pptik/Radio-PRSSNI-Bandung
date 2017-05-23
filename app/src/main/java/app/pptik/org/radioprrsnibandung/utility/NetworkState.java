1e406ad09d40ad12248592a5b2e03a1dec2fa3f6
yusfia <yusfia.hafidz@gmail.com>
2017-05-23 17:46:52 +0700

    Modification on code

--- app/src/main/java/app/pptik/org/radioprrsnibandung/utility/NetworkState.java
+++ app/src/main/java/app/pptik/org/radioprrsnibandung/utility/NetworkState.java
@@ -5,6 +5,8 @@ import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.widget.Toast;
 
+import app.pptik.org.radioprrsnibandung.R;
+
 /**
  * Created by Hafid on 5/16/2017.
  */
@@ -14,7 +16,7 @@ public class NetworkState {
         ConnectivityManager cm = (ConnectivityManager) ctx.getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo netInfo = cm.getActiveNetworkInfo();
         boolean connect = netInfo != null && netInfo.isConnectedOrConnecting();
-        if (!connect) Toast.makeText(ctx, "Internet service not available!", Toast.LENGTH_LONG).show();
+        if (!connect) Toast.makeText(ctx, R.string.internet_not_available, Toast.LENGTH_LONG).show();
         return connect;
     }
 }
